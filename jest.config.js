38e2b22137522efdc7b15cacdd08ff2fa41acfba
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2023-11-30 19:26:17 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,30 @@
-// jest.config.js
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  roots: ['<rootDir>/src'],
+  modulePaths: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageReporters: ['lcov', 'text', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testTimeout: 30000, 
+  maxConcurrency: 5, 
+  verbose: true,
 };
 
+export default config;
